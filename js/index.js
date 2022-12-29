fd41656cd3bd3933637a4efc49b73c7de5f47e96
hosnahoseini <hosna.hoseini@gmail.com>
2022-12-29 14:30:27 +0330

    [feat] responsiveness with JS

--- js/index.js
+++ js/index.js
@@ -1,29 +1,70 @@
 const nameInput = document.querySelector('#name');
 const submitButton = document.querySelector('.submit');
 
+const userInfo = document.querySelector('.user_info');
+const userBio = document.querySelector('.user_bio');
+
+const actionResult = document.querySelector('.action_result');
+
+
+
 // send request to server and get data for input name then call functions to show result
-async function getGender(e) {
+async function getInfo(e) {
     let name = nameInput.value;
     e.preventDefault();
     if (checkValidity(name)) {
         let data = await JSON.parse(window.localStorage.getItem(name));
-
+        console.log("local storage")
+        console.log(data)
         if (data == null){
             try {
                 let response = await fetch(`https://api.github.com/users/${name}`);
                 let data = await response.json();
-                if (response.status != 200) {
+                console.log("api")
+                console.log(data)
+                if (response.status == 404) {
+                    showAlert("User not found");
+                    return;
+                }
+                else if (response.status != 200) {
+                    showAlert("User not found");
                     return Promise.reject(`Request failed with error ${response.status}`);
                 }
+                else{
                 window.localStorage.setItem(name, JSON.stringify(data));
+                }
             } catch (e) {
                 console.log(e);
             }
         }
+
+        setPrediction(data);
+        showAlert("User found");
+
     } else {
         showAlert("Invalid input!");
     }
 }
+
+// show Prediction result to user
+function setPrediction(obj) {
+    console.log("setting ...")
+    if (obj != null) {
+        userInfo.innerHTML = `
+                            <img id="avatar" src=${obj.avatar_url} alt="Italian Trulli">
+                            <div id="text_info">
+                            <p><span class="title">Name: </span>${obj.name}</p>
+                            <p><span class="title">Blog: </span><a href=${obj.blog}>${obj.blog}</a></p>
+                            <p><span class="title">Location: </span>${obj.location} </p>
+                            </div>
+                            `
+        userBio.innerHTML = `<pre><span class="title">Bio: </span>${obj.bio} </pre>`
+        findPopularLang(obj.name);
+    } else {
+        showAlert("Can't Find!");
+    }
+}
+
 
 // this function check input name validity
 function checkValidity(name) {
@@ -34,7 +75,7 @@ function checkValidity(name) {
     if (foundNotValid == null && foundValid.length > 0) {
         return true;
     }
-    return false;
+    return true;
 
 }
 
@@ -47,5 +88,5 @@ function showAlert(title) {
     }, 4000);
 }
 
-submitButton.addEventListener('click', getGender);
-window.localStorage.clear();
+submitButton.addEventListener('click', getInfo);
+// window.localStorage.clear();
